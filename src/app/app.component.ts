e8cb7aa376199622cf98ea8796f1740ab90fd124
mohit-rathi176 <mohitrathi176@gmail.com>
2025-02-16 14:10:43 +0530

    Initialize project with primeng and tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-forms';
+
 }
